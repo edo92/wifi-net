216829e6b25a3b6b06596435cca1425cce4d699c
Eduard Jacobs <edoter92@gmail.com>
2020-08-10 17:05:03 -0700

    :bookmark: update readme

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
     entry: './src/index.ts',
+    target: 'node',
     module: {
         rules: [
             {
